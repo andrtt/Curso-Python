bd350b0af2d01f3e65dda508ccf3ea897d0a8dda
Natã Andretto <nhandretto@gmail.com>
2024-10-18 02:41:16 -0300

    input
    aprendendo a usar inputs

---
+++ aula17_input.py
@@ -0,0 +1,15 @@
+# nome = input('Qual o seu nome? ')
+# print(f'O seu nome é {nome}')
+
+nome = input('O seu nome é: ')
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+# transformanto o input numero_1 e numero_2 que são strings em inteiro. Todo input é uma str.
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A soma dos números é: {int_numero_1 + int_numero_2}')
+
+print (f'O seu nome é {nome}')
